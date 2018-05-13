b12dc99900633278b0173c279d88c2f8d2198592
Yoichi Kawasaki <yoichika@microsoft.com>
2018-05-13 16:52:13 +0900

    modified not to use perl inplace mode as it does not work in some environment

--- scripts/setup-logicapp.sh
+++ scripts/setup-logicapp.sh
@@ -19,11 +19,15 @@ TEMPLATE_FILE=$cwd/../logicapp/LogicApp.json
 PARAM_TEMPLATE_FILE=$cwd/../logicapp/LogicApp.parameters.json
 PARAM_FILE=$cwd/../logicapp/_LogicApp.parameters.json
 
-cp $PARAM_TEMPLATE_FILE $PARAM_FILE
-
 # Set variables
-perl -p -i -e "s,{logicAppName},$LOGICAPP_NAME,g" $PARAM_FILE
-perl -p -i -e "s,{webhookSubscribeEndpoint},$WEBHOOK_ENDPOINT,g" $PARAM_FILE
+## [NOTE]
+##   Change to use replacing values on the fly from inplace mode 
+##   as inplace mode (-i) doesn't work on some environment
+#perl -p -i -e "s,{logicAppName},$LOGICAPP_NAME,g" $PARAM_FILE
+#perl -p -i -e "s,{webhookSubscribeEndpoint},$WEBHOOK_ENDPOINT,g" $PARAM_FILE
+cat $PARAM_TEMPLATE_FILE | \
+        perl -p -e "s,{logicAppName},$LOGICAPP_NAME,g"  | \
+        perl -p -e "s,{webhookSubscribeEndpoint},$WEBHOOK_ENDPOINT,g" > $PARAM_FILE
 
 az group deployment create --name $LOGICAPP_NAME \
      --resource-group $RESOURCE_GROUP \
